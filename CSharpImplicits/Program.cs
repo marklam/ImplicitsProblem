a4d06bcc1277e1e2d047349ed428d7d0bb4277f0
Mark Lambert <marklam@spunow.co.uk>
2022-04-05 19:33:03 +0100

    Added example of C#'s use of implicits

---
+++ CSharpImplicits/Program.cs
@@ -0,0 +1,30 @@
+﻿using Pulumi;
+using Pulumi.AzureNative.Cdn.Inputs;
+
+namespace CSharpImplicits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var matchValues = new InputList<string>
+            {
+                "HTTP"
+            };
+
+            // This version
+            //IL_0023: newobj instance void [Pulumi.AzureNative]Pulumi.AzureNative.Cdn.Inputs.DeliveryRuleRequestSchemeConditionArgs::.ctor()
+            //IL_0028: dup
+            var conditionImplicit =
+                new DeliveryRuleRequestSchemeConditionArgs {
+                    Name = "RequestScheme1",
+                    Parameters =
+                        new RequestSchemeMatchConditionParametersArgs {
+                            MatchValues = matchValues,
+                            Operator = "Equal",
+                            OdataType = "#Microsoft.Azure.Cdn.Models.DeliveryRuleRequestSchemeConditionParameters"
+                        }
+                };
+        }
+    }
+}
